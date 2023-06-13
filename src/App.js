e3c39e06d720cfa95a03fb422b75473001ba706b
MeiyaRam <MeiyaRam@gmail.com>
2023-06-13 12:54:44 +0530

    Modified App file for move components call to Todopane.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import { React } from 'react';
 import './App.scss';
-import Input from './components/Input';
-import AddButton from './components/AddButton';
-import Todos from './components/Todos';
-import FilterBar from './components/FilterBar';
+import TodoPane from './components/TodoPane';
 
 const App = (context) =>
 	<div className="App">
 		ToDo App
-		<Input { ...context }/>
-		<AddButton { ...context }/>
-		<Todos { ...context }/>
-		<FilterBar { ...context }/>
+		<TodoPane { ...context }/>
 	</div>;
 
 export default App;
